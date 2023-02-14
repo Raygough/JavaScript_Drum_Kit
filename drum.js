2a76dde3dac13ce00a3662ede7569e980c16cfd0
Raygough <rrg3711@gmail.com>
2023-02-13 22:00:36 -0500

    final commit

--- drum.js
+++ drum.js
@@ -1,8 +1,19 @@
 window.addEventListener('keypress', function (e) {
     console.log(e.code);
     const audio = document.querySelector(`audio[data-key = "${e.code}"]`);
+    const key = document.querySelector(`.key[data-key = "${e.code}"]`);
     console.log(audio);
-    if(!audio) return;
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+    if (!audio) return;
     audio.currentTime = 0;
-    audio.play();
+    audio.play()
+    key.classList.add('playing');
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 });
